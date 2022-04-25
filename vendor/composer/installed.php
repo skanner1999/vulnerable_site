85b6d449a8b29dd483db8977ade2dca319dc104d
skanner2 <63942471+skanner2@users.noreply.github.com>
2022-04-25 14:17:00 -0400

    composer fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a0c658e7ff9761e6147d6a513d7ad8940b28430c',
+        'name' => 'your-vendor-name/package-name',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-vendor-name/package-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a0c658e7ff9761e6147d6a513d7ad8940b28430c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
